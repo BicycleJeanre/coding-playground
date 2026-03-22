c60526a0891eb1cdd92c933710f0a1df8347e541
Bicycle-Jeanre <jeanre@bicyclebi.com>
2026-03-22 15:24:37 +0200

    adding first py cli test

---
+++ cli-pixels.py
@@ -0,0 +1,17 @@
+
+#setup the width and height of window in characters
+width = 250
+height = 20
+
+# print top line
+header = ""
+for i in range(width):header+="_"
+print(header)
+
+#print height
+line="|"
+
+for i in range(width): line+=" "
+line+="|"
+
+
